f756be142eed6af7c75df60f403e50f413bb3b75
luiz_gf_ortiz <luiz_gf_ortiz@estudante.sesisenai.org.br>
2026-03-16 14:37:27 -0300

    docs: adiciona configuração global do OpenAPI com informações do autor

---
+++ src/main/java/com/ctw/monitoramentoagricola/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.ctw.monitoramentoagricola.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Monitoramento Agrícola API")
+                        .version("1.0.0")
+                        .description("API para monitoramento de sensores de temperatura, umidade, pressão e luminosidade em estufas.")
+                        .contact(new Contact()
+                                .name("Luiz Guilherme")
+                                )
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://springdoc.org")));
+    }
+}
